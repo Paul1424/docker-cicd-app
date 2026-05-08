66ef7145300c0bb433f251bc38df5c2594231bcf
Anand Paul <devrapallichinnu14@gmail.com>
2026-05-08 08:36:24 +0530

    Added blue background

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template_string
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from GitHub Actions CI/CD Pipeline!"
+    with open('index.html', 'r') as f:
+        content = f.read()
+    return content
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
